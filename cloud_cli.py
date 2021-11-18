bf7b5d724f454e4bf70891e26b5f79fa862a3942
Rahul Sharma <sharmaR0810@gmail.com>
2021-11-18 18:02:23 +0530

    Code for review

--- cloud_cli.py
+++ cloud_cli.py
@@ -1,14 +1,28 @@
 import click
-from src.vm_instance import vm as vmops
+from src.vm_instance.vm import ls_vm
 
 
 @click.group()
 def entry_point():
-    """ CLI EntryPoint """
     pass
 
+@entry_point.command()
+@click.option('--proj', required=True, type=str,
+              help='GCP Project ID', default = "")
+@click.option('--filt', required=True, type=str,
+              help='Name or regex of instance name', defualt = "")
+# def ls_vm(proj, filt):
+#     """List instances Based on filters"""
+def list_vm(proj, filt):
+    ls_vm(proj, filt)
 
-entry_point.add_command(vmops.list_instances_by_filter)
+entry_point.add_command(ls_vm)
 
 if __name__ == "__main__":
-    entry_point()
+    entry_point(default_map = {
+        "list_vm":{
+            "proj":
+        }
+    })
+
+#coming in 2 minutes you can take control
--- src/vm_instance/vm.py
+++ src/vm_instance/vm.py
@@ -7,7 +7,9 @@ from oauth2client.client import GoogleCredentials
 credentials = GoogleCredentials.get_application_default()
 service = discovery.build('compute', 'v1', credentials=credentials)
 
-def list_instances_by_filter(project, filter):
+
+@click.command()
+def ls_vm(proj, filt):
     """
     To list instances from a GCP project based on filters.
     ---
@@ -19,17 +21,24 @@ def list_instances_by_filter(project, filter):
     Return: None
     """
     request = service.instances().aggregatedList(
-        project=project, filter=filter)
+        project=proj, filter=filt)
     with open('resource.json', 'w', encoding='utf-8') as file:
+        # Create a key to initialize a dictionary of instances.
         json_key = {'response': []}
         while request is not None:
             response = request.execute()
-
             instance = response.get('items', {})
             for instance in instance.values():
                 for instances in instance.get('instances', []):
                     json_key.get('response').append(instances)
-                # Print is working, Head output is
+            # Appending previous response on next request
             request = service.instances().aggregatedList_next(
                 previous_request=request, previous_response=response)
         json.dump(json_key, file)
+
+
+# proj = 'wise-arch-107501'
+# filt = "name eq '^longlive-honeywell-proxy.*'"
+
+# if __name__ == '__main__':
+#     ls_vm(proj, filt)
